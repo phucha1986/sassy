bfd6e0495970d08506364a4717780b805f6b4b62
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-01-06 18:41:34 -0300

    refactor: fix erros build

--- src/app/(auth)/_components/BackToLandingPage.tsx
+++ src/app/(auth)/_components/BackToLandingPage.tsx
@@ -1,13 +1,13 @@
+import Link from 'next/link';
 import { ArrowLeftIcon } from '@heroicons/react/24/solid';
 
 export default function BackToLandingPage() {
     return (
-        <a
-            href="/"
-            className="flex items-center text-indigo-600 hover:text-indigo-700 font-medium"
-        >
-            <ArrowLeftIcon className="h-5 w-5 mr-1" />
-            Back to Home
-        </a>
+        <Link href="/">
+            <a className="flex items-center text-indigo-600 hover:text-indigo-700 font-medium">
+                <ArrowLeftIcon className="h-5 w-5 mr-1" />
+                Back to Home
+            </a>
+        </Link>
     );
 }
