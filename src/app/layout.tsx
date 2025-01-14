5db8295ea9ffa01aa7c8aee13bdb85c7441fbecf
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-01-13 22:11:26 -0300

    feat: add backup font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Poppins } from 'next/font/google'
 import localFont from 'next/font/local'
 
 import Toast from '@/components/Toast';
@@ -8,6 +9,10 @@ import "@/styles/globals.css";
 
 
 const myFont = localFont({ src: '../../public/Satoshi.ttf' })
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100','200','300','400','500','600','700','800','900',]
+})
 
 type Props = {
   children: React.ReactNode;
@@ -16,7 +21,7 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <meta name="google-site-verification" content="hOwFMK8Nm7tKqU6eOyDCH3EUk6u6kht_Zpzqoucazgw" />
-      <body className={myFont.className}>
+      <body className={myFont.className || poppins.className}>
         <ToastProvider>
           {children}
           <Toast />
