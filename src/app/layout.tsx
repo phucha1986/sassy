8b8995ebff44d5086b8386bb63ecf7ec5e7bd76f
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-01-27 15:14:05 -0300

    Hotfix: Fix locale fetching and static files issue (#13)
    * feat: init base of feature
    
    * chore: remove loading only in fail in signin
    
    * feat: add i18n
    
    * feat: update roadmap docs
    
    * feat: update github templates
    
    * feat: add i18n in mobile
    
    * refactor: create navbar dashboard component and add languague selector in dashboard
    
    * chore: update future roadmap
    
    * chore: create issue finalized markdown template
    
    * fix: add vercel json to use public in static
    
    * fix: get locale in production with fetch
    
    * chore: remove vercel file
    
    * fix: moving locales to src
    
    * chore: move location to src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,43 @@
-import fs from 'fs';
-import path from 'path';
-
 import { cookies } from 'next/headers';
 
-import { Poppins } from 'next/font/google'
+import { Poppins } from 'next/font/google';
 
 import Toast from '@/components/Toast';
 import { DatadogProvider } from '@/contexts/DatadogContext';
 import { I18nProvider } from '@/contexts/i18nContext';
 import { ToastProvider } from '@/contexts/ToastContext';
+import enUSLocale from '@/locales/en-US.json';
+import ptBRLocale from '@/locales/pt-BR.json';
 
 import "@/styles/globals.css";
 
-
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900',]
-})
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
 
 export async function generateStaticParams() {
   return [{ locale: 'en-US' }, { locale: 'pt-BR' }, { locale: 'es' }];
 }
 
 const loadTranslations = (locale: string): Record<string, string> => {
-  const filePath = path.join(process.cwd(), 'public', 'locales', `${locale}.json`);
-  const file = fs.readFileSync(filePath, 'utf-8');
-  return JSON.parse(file);
+  const translationsMap: Record<string, Record<string, string>> = {
+    'en-US': enUSLocale,
+    'pt-BR': ptBRLocale,
+  };
+
+  const translations = translationsMap[locale] || enUSLocale;
+  return translations;
 };
 
 type Props = {
   children: React.ReactNode;
-}
+};
+
 export default async function RootLayout({ children }: Props) {
   const cookieStore = await cookies();
   const locale = cookieStore.get('locale')?.value || 'en-US';
-  const translations = loadTranslations(locale);
+  const translations = await loadTranslations(locale);
 
   return (
     <html lang={locale}>
