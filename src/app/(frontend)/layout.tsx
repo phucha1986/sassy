b0b9fef67b4e178cf6995d50914a013b4afd7d3a
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-04-29 01:07:47 -0300

    Feat/add jest (#29)
    * hotfix: update capitalize function do valid empty
    
    * chore: add jest base

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,20 +1,20 @@
-import { Poppins } from 'next/font/google';
+import { Poppins } from "next/font/google";
 
-import Toast from '@/components/Toast';
-import { DatadogProvider } from '@/contexts/DatadogContext';
-import { I18nProvider } from '@/contexts/i18nContext';
-import { ToastProvider } from '@/contexts/ToastContext';
-import { loadTranslationsSSR } from '@/utils/loadTranslationsSSR';
+import Toast from "@/components/Toast";
+import { DatadogProvider } from "@/contexts/DatadogContext";
+import { I18nProvider } from "@/contexts/i18nContext";
+import { ToastProvider } from "@/contexts/ToastContext";
+import { loadTranslationsSSR } from "@/utils/loadTranslationsSSR";
 
 import "@/styles/globals.css";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export async function generateStaticParams() {
-  return [{ locale: 'en-US' }, { locale: 'pt-BR' }, { locale: 'es' }];
+  return [{ locale: "en-US" }, { locale: "pt-BR" }, { locale: "es" }];
 }
 
 type Props = {
@@ -26,7 +26,10 @@ export default async function RootLayout({ children }: Props) {
 
   return (
     <html lang={locale}>
-      <title>{translate('title')}</title>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>{translate("title")}</title>
+      </head>
       <body className={poppins.className}>
         <I18nProvider locale={locale} translations={translations}>
           <DatadogProvider>
