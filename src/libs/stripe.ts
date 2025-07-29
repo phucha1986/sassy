5aa1de7fd3056fd9b69222805e0de7602db3beff
phucha1986 <hahanhphuc1986@gmail.com>
2025-07-30 00:06:35 +0400

    fix

--- src/libs/stripe.ts
+++ src/libs/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: '2025-03-31.basil',
+    apiVersion: '2025-06-30.basil',
 });
 
